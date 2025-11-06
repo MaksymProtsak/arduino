47164744479c6e4322438c0fa75908fd94e7c3e9
MaksymProtsak <maksymprotsak@di.local>
2025-11-06 08:30:23 +0200

    Created DeviceData. Updated code with DeviceData.

--- NodeMCU_telegram_bot_blinc/NodeMCU_telegram_bot_blinc.ino
+++ NodeMCU_telegram_bot_blinc/NodeMCU_telegram_bot_blinc.ino
@@ -1,9 +1,11 @@
-#include<EEPROM.h>
-#include<ESP8266WiFi.h>
-#include<WiFiClientSecure.h>
-#include<UniversalTelegramBot.h>
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
 
-#define EEPROM_SIZE 512
+#define EEPROM_SIZE 4096
+#define MAX_CHAT_IDS 100
+#define DEVICE_NAME_ADDR 0
 
 const char* ssid="OnePlus 13R A83E";
 const char* password="max123456";
@@ -16,68 +18,92 @@ const char* commandLedOn = "/led_on";
 const char* commandLedOff = "/led_off";
 const char* commandGetChipId = "/get_chip_id";
 const char* commandDevice = "/device";
+const char* commendForeverSleepOn = "/foreverSleepOn";
+const char* commendForeverSleepOff = "/foreverSleepOff";
 
 bool ledPinStatus = HIGH;
 int LedPin = 2;
 String startMessage = "This is WiFi button.";
 String devName;
+bool foreverSleep;
 
 WiFiClientSecure secured_client;
 UniversalTelegramBot bot(bot_token, secured_client);
+
+struct DeviceData {
+  char wifiSSID[64];
+  char wifiPassword[64];
+  char deviceName[256];  // 124 кириличних символи
+  bool deepSleepEnabled;
+  int64_t chatIds[MAX_CHAT_IDS];
+  int chatIdCount;
+};
+
+DeviceData data;
 
 void setup() {
   pinMode(LedPin, OUTPUT);
   digitalWrite(LedPin, ledPinStatus);
 
-  EEPROM.begin(512);
+  EEPROM.begin(EEPROM_SIZE);
   String EEPROMresponse = readFromEEPROM();
-  if (EEPROMresponse){
+  if (EEPROMresponse) {
     devName = EEPROMresponse;
     startMessage = devName;
   }
   Serial.begin(115200);
 
   WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
 
   secured_client.setInsecure();
   bot.sendMessage(chatId, startMessage, "");
-  // ESP.deepSleep(0);
+  if (foreverSleep) {
+    ESP.deepSleep(0);
+  }
 }
 
-int writeInEEPROM(const String & text){
-  int len = text.length();
-  if (len >= EEPROM_SIZE | len == 0){
+int writeInEEPROM(const String& text) {
+  size_t maxLen = sizeof(data.deviceName) - 1;
+  size_t len = text.length() * 2;
+  
+  if (len >= maxLen || len == 0) {
     return -1;
-    }
+  }
 
-  if (text == readFromEEPROM()){
+  if (text == readFromEEPROM()) {
     return 0;
   }
 
-  for (int i = 0; i < text.length(); ++i) {
-    EEPROM.write(i, text[i]);
+  for (size_t i = 0; i < len; i=i+1){
+    data.deviceName[i] = text[i];
   }
-  EEPROM.write(text.length(), '\0');
+  data.deviceName[len] = '\0';
+
+  for (size_t i = 0; i < sizeof(data.deviceName); ++i) {
+    EEPROM.write(DEVICE_NAME_ADDR + i, data.deviceName[i]);
+  }
+  
   EEPROM.commit();
   return 1;
 }
 
-String readFromEEPROM(){
+String readFromEEPROM() {
   String result = "";
-  for (int i=0; i <EEPROM_SIZE; i=i+1){
+  for (int i = 0; i < EEPROM_SIZE; i = i + 1) {
     char c = EEPROM.read(i);
+
     if (c == '\0') break;
     result += c;
   }
   return result;
 }
 
-void parseMessageText(int newMessages){
-    for (int i = 0; i < newMessages; i=i+1){
-      String messageText = bot.messages[i].text;
+void parseMessageText(int newMessages) {
+  for (int i = 0; i < newMessages; i = i + 1) {
+    String messageText = bot.messages[i].text;
 
       if (messageText == commandStart){
         String response =
@@ -91,59 +117,58 @@ void parseMessageText(int newMessages){
           chatId, response, "");
       }
 
-      if (messageText == commandLedOn){
-        ledPinStatus = LOW;
-        bot.sendMessage(chatId, F("LED IS ON"), "");
-        digitalWrite(LedPin, ledPinStatus);
+    if (messageText == commandLedOn) {  // /led_on
+      ledPinStatus = LOW;
+      bot.sendMessage(chatId, F("LED IS ON"), "");
+      digitalWrite(LedPin, ledPinStatus);
+      return;
+    }
+
+    if (messageText == commandLedOff) {  // led_off
+      ledPinStatus = HIGH;
+      bot.sendMessage(chatId, F("LED IS OFF"), "");
+      digitalWrite(LedPin, ledPinStatus);
+      return;
+    }
+
+    if (messageText == commandGetChipId) {  // get_chip_id
+      bot.sendMessage(chatId, String(chipId), "");
+      return;
+    }
+
+    if (messageText.indexOf(String(chipId)) != -1) {
+      if (messageText == "/" + String(chipId)) {
+        bot.sendMessage(chatId, devName, "");
         return;
       }
 
-      if (messageText == commandLedOff){
-        ledPinStatus = HIGH;
-        bot.sendMessage(chatId, F("LED IS OFF"), "");
-        digitalWrite(LedPin, ledPinStatus);
-        return;
-      }
-      
-      if (messageText == commandGetChipId){
-        bot.sendMessage(chatId, String(chipId), "");
-        return;
-      }   
-      
-      if (messageText.indexOf(String(chipId)) != -1){
-        if (messageText == "/" + String(chipId)){
-          bot.sendMessage(chatId, devName, "");
-          return;
-        }
+      if (messageText.indexOf(String(commandDevice) + String(chipId)) != -1) {
+        int underscoreIndex = messageText.indexOf('_');
 
-        if (messageText.indexOf(String(commandDevice) + String(chipId)) != -1){
-          int underscoreIndex = messageText.indexOf('_');
-
-          if (underscoreIndex != -1){
-            devName = messageText.substring(underscoreIndex + 1);
-            int result = writeInEEPROM(devName);
-            if (result == -1){
-              bot.sendMessage(chatId, "Помилка назви.", "");
-            }
-            if (result == 0){
-              bot.sendMessage(chatId, "Назва не змінена.", "");
-            }
-            if (result == 1){
-              bot.sendMessage(chatId, "Назва успішно змінена!", "");
-            }
+        if (underscoreIndex != -1) {
+          devName = messageText.substring(underscoreIndex + 1);
+          int result = writeInEEPROM(devName);
+          if (result == -1) {
+            bot.sendMessage(chatId, "Помилка назви.", "");
           }
-          else{
-            bot.sendMessage(chatId, "Не вказано розділовий знак `_`", "");
+          if (result == 0) {
+            bot.sendMessage(chatId, "Назва не змінена.", "");
           }
+          if (result == 1) {
+            bot.sendMessage(chatId, "Назва успішно змінена!", "");
+          }
+        } else {
+          bot.sendMessage(chatId, "Не вказано розділовий знак `_`", "");
         }
       }
     }
+  }
 }
 
 void loop() {
   int lastMessageId = bot.last_message_received + 1;
   int newMessages = bot.getUpdates(lastMessageId);
-  if (newMessages){
+  if (newMessages) {
     parseMessageText(newMessages);
   }
 }
